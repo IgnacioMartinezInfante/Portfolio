1fb09be594d1cce81ea15bff2541516011a79c1e
IgnacioMartinezInfante <ignacio.martinezinfante@gmail.com>
2026-03-13 19:02:19 -0300

    Hover cursor videos youtube

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-document.querySelectorAll(".project-video").forEach(video => {
+document.querySelectorAll(".project-video").forEach(container => {
 
-  video.addEventListener("click", () => {
+  const videoId = container.dataset.video
+  let iframeLoaded = false
 
-    const id = video.dataset.video;
+  function loadVideo() {
 
-    video.innerHTML = `
-      <iframe
-      src="https://www.youtube.com/embed/${id}?autoplay=1"
-      frameborder="0"
-      allow="autoplay; encrypted-media"
-      allowfullscreen>
-      </iframe>
-    `;
+    if (iframeLoaded) return
 
-  });
+    const iframe = document.createElement("iframe")
 
-});
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}`
+    iframe.frameBorder = "0"
+    iframe.allow = "autoplay; fullscreen"
+    iframe.allowFullscreen = true
+
+    container.innerHTML = ""
+    container.appendChild(iframe)
+
+    iframeLoaded = true
+  }
+
+  function unloadVideo() {
+
+    if (!iframeLoaded) return
+
+    const img = document.createElement("img")
+    img.src = container.dataset.thumbnail
+
+    const play = document.createElement("div")
+    play.className = "play-button"
+    play.innerHTML = "▶"
+
+    container.innerHTML = ""
+    container.appendChild(img)
+    container.appendChild(play)
+
+    iframeLoaded = false
+  }
+
+  container.dataset.thumbnail = container.querySelector("img").src
+
+  container.addEventListener("mouseenter", loadVideo)
+  container.addEventListener("mouseleave", unloadVideo)
+
+  container.addEventListener("click", loadVideo)
+
+})
