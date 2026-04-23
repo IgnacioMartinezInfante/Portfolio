08befe6ff0a2966171b2e4d26b231a471d3d6c01
IgnacioMartinezInfante <ignacio.martinezinfante@gmail.com>
2026-04-23 17:03:24 -0300

    animacion scroll y "cards"  en texto del mon

--- script.js
+++ script.js
@@ -118,4 +118,16 @@ if (nextBtn) {
   };
 }
 
+const elements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
+
 });
